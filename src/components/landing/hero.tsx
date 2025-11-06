1a3a815b6d461ac3a005927427a2177d5563f8f9
Jordan Garcia Jara <jordangarciajara@gmail.com>
2025-11-06 11:34:26 +0000

    REDUCE OTROS 10 PX A LA DERECHA

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -17,8 +17,8 @@ export function Hero() {
       <div className="absolute inset-0 bg-black/60" />
       <div className="relative container mx-auto flex flex-col items-center justify-center h-full px-4">
         <div className="max-w-4xl mx-auto mb-8 text-center">
-           <Link href="/" className="flex items-center justify-center gap-1">
-              <div className="relative h-24 w-[126px] sm:h-32 sm:w-[168px]">
+           <Link href="/" className="flex items-center justify-center gap-1 sm:gap-2">
+              <div className="relative h-24 w-[106px] sm:h-32 sm:w-[138px]">
                   <Image
                       src="https://iili.io/KQ5UldN.png"
                       alt="Macout Logo Manzana"
@@ -26,7 +26,7 @@ export function Hero() {
                       className="object-contain"
                   />
               </div>
-              <div className="relative h-[100px] w-[225px] sm:h-[150px] sm:w-[325px]">
+              <div className="relative h-[100px] w-[200px] sm:h-[150px] sm:w-[325px]">
                 <Image
                     src="https://iili.io/KQ5Y5vt.png"
                     alt="Macout Logotipo"
